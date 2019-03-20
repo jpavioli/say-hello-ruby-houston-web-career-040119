5eb9cc89283758a037017f2abe439080fa7e472d
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-20 16:10:14 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,7 @@
 # Build your say_hello method here
 
 #say_hello accepts an argument of a name and prints out Hello with that name
-define say_hello(name)
+def say_hello(name)
+  puts "Hello #{name}!"
+end
+
